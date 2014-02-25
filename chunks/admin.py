aa70e70996e365fa77cf51fba20f69406d6f172a
Daniel Johansson <donnex@donnex.net>
2014-02-25 20:17:19 +0100

    Add name to admin list_display and search_fields. Wrap output in chunk class

--- chunks/admin.py
+++ chunks/admin.py
@@ -3,8 +3,8 @@ from models import Chunk
 
 
 class ChunkAdmin(admin.ModelAdmin):
-    list_display = ('key', 'description',)
-    search_fields = ('key', 'content')
+    list_display = ('name', 'key',)
+    search_fields = ('name', 'key', 'content')
     prepopulated_fields = {'key': ('name',)}
 
 admin.site.register(Chunk, ChunkAdmin)
