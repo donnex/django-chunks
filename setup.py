282ae9514fe3fb4f930d1265065d0f002f06e908
Daniel Johansson <donnex@donnex.net>
2014-05-10 20:34:24 +0200

    Version 0.4.1
    - Added locale to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='django-chunks',
-      version='0.4.0',
+      version='0.4.1',
       description='Keyed blocks of content for use in your Django templates',
       author='Clint Ecker',
       author_email='me@clintecker.com',
       url='http://code.google.com/p/django-chunks/',
-      packages=['chunks', 'chunks.templatetags'],
+      packages=['chunks', 'chunks.templatetags', 'chunks.locale'],
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Intended Audience :: Developers',
